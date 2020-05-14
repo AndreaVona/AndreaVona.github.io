30c91620f50994b233e5d06f0c751f035475a8ee
ANDREA VONA <andrea.vona23@gmail.com>
2020-05-14 11:59:29 +0200

    ok

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-$.getJSON('https://ipapi.co/json/', function(data) {
+/*$.getJSON('https://ipapi.co/json/', function(data) {
   console.log(JSON.stringify(data, null, 2));
-});
+}); */
 
 
 // Create a client instance
