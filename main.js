d9a08e0d1b6fb44ee422b589032cb7e35a2267c0
ANDREA VONA <andrea.vona23@gmail.com>
2020-05-19 19:05:13 +0200

    refactor

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-let container = document.getElementById('container');
+let x = document.getElementById('x');
+let y = document.getElementById('y');
+let z = document.getElementById('z');
 let activity = document.getElementById('activity');
 if ( 'Accelerometer' in window ) {
   let sensor = new LinearAccelerationSensor();
   var newMeasurement = {};
 
   sensor.addEventListener('reading', function(e) {
-  container.innerHTML = '<div class=\"row\"><div class=\"col-sm text-center\">x: ' + e.target.x + '</div><div class=\"col-sm text-center\"> y: ' + e.target.y + '</div><div class=\"col-sm text-center\"> z: ' + e.target.z + '</div></div>';
-  
+  //container.innerHTML = '<div class=\"row\"><div class=\"col-sm text-center\">x: ' + e.target.x + '</div><div class=\"col-sm text-center\"> y: ' + e.target.y + '</div><div class=\"col-sm text-center\"> z: ' + e.target.z + '</div></div>';
+  x.innerHTML = "x: " + e.target.x;
+  y.innerHTML = "x: " + e.target.y;
+  z.innerHTML = "x: " + e.target.z;
   /* 
    <div class="row">
     <div class="col-sm">
@@ -29,9 +33,9 @@ if ( 'Accelerometer' in window ) {
   var z = newMeasurement.z;
   var movement = Math.sqrt(Math.pow(newMeasurement.x, 2) + Math.pow(newMeasurement.y, 2) + Math.pow(z, 2));
   if(movement>0.75) {
-	activity.innerHTML = '<div class=\"row alert alert-success text-center\"> Walking </div>';
+	activity.innerHTML = '<div class=\"row alert alert-success text-center\"><div class=\"col-lg\"> Walking </div></div>';
   } else {
-	activity.innerHTML = '<div class=\"row alert alert-info text-center\"> Standing still </div>';
+	activity.innerHTML = '<div class=\"row alert alert-info text-center\"><div class=\"col-lg\"> Standing still </div></div>';
   }
   var msgEdge = JSON.stringify(message);
 
